7554cd71c36a77d11b90eb71ff999d94f06f3316
admiralwaffle4 <56054320+admiralwaffle4@users.noreply.github.com>
2022-10-05 20:33:39 -0400

    promptai commands

--- bot.py
+++ bot.py
@@ -4,32 +4,18 @@ import os
 from discord import channel
 from discord.ext import commands
 from discord.commands import Option
+from discord.ui import *
 from googlesearch import search
 from dotenv import load_dotenv
 from random import randint, choice
 from datetime import datetime
-
-
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def main():
-  return "Your Bot Is Ready"
-
-def run():
-  app.run(host="0.0.0.0", port=8000)
-
-def keep_alive():
-  server = Thread(target=run)
-  server.start()
+import openai
   
 #dotenv my beloved and also the iniatlizing of the bot itself
 load_dotenv()
-bot = discord.Bot(debug_guilds=[866756514996158474,899047123415343114,992568762840653897])
+bot = discord.Bot(debug_guilds=[866756514996158474,899047123415343114,992568762840653897,787120261437718539])
 TOKEN = os.getenv("TOKEN")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #readying and installation
 #tells me the bot is online in the terminal
@@ -42,10 +28,21 @@ async def on_ready():
 async def ping(ctx):
     await ctx.respond(f"Pong! Latency is {bot.latency}")
 
-@bot.command(description="Asks an anonymous question to staff.")
-async def ask(ctx,question: Option(str, "Enter your question.")):
-    await ctx.respond(f"Your question ({question}) has been sent to staff.",ephemeral=True)
-    channel = bot.get_channel(992569868509520002)
-    await channel.send(f"{ctx.author} has sent a question reading {question}.")
-
+@bot.command(description="Generates a result based on the prompt.")
+async def prompt(ctx,promptai: Option(str,"Your prompt for the AI"),temp: Option(float,"The risk level of your query from 0 to 1.",required=False,default=.5)):
+    try:
+        if temp > 1:
+            temp = 1
+        elif temp < 0:
+            temp = 0
+        response = openai.Completion.create(
+          model="text-davinci-002",
+          prompt=promptai,
+          temperature=temp,
+          max_tokens=100
+         )
+        await ctx.respond(response.choices[0].text)  
+    except:
+          await ctx.respond("I am currently unavailable.")
+  
 bot.run(TOKEN)
